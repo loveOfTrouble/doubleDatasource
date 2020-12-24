2a63248243d0b72b624c08e8ddebc8b7a2089c05
zhuzx <1061265062@qq.com>
2020-12-24 14:34:45 +0800

    完成上传文件的接口（第一次修改）

---
+++ src/main/java/com/sinosoft/doubledatasource/start/Start.java
@@ -0,0 +1,24 @@
+package com.sinosoft.doubledatasource.start;
+
+import com.sinosoft.doubledatasource.service.PhotoService;
+import com.sinosoft.doubledatasource.service.task.MaterialTask;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class Start implements ApplicationRunner {
+
+    @Autowired
+    private MaterialTask materialTask;
+
+    @Autowired
+    private PhotoService photoService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        materialTask.syncMaterial();
+
+        photoService.synchronizeCadreFilePhoto();
+    }
+}
